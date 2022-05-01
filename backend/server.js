dfa493d6fd91e8bac60dff494128f8f47fda1135
prashant <prashant32123kumar@gmail.com>
2022-05-02 03:15:26 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(express.json()); //to accept json data
 dotenv.config();
 connectDB();
-// app.get("/", (req, res) => {
-//   res.send("API is Successfully running");
-// });
+app.get("/", (req, res) => {
+  res.send("API is Successfully running");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -21,19 +21,19 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // --------------------------deployment------------------------------
 
