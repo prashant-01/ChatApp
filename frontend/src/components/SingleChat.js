460edfd67cee6fce31630d84c86437e372a86df6
prashant <prashant32123kumar@gmail.com>
2022-05-02 02:44:06 +0530

    make it better

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "https://chat-on-chat.herokuapp.com/";
+const ENDPOINT = "https://chit-n-chat.herokuapp.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
